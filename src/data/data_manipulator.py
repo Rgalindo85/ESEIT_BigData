1f0178e7071a2c2bc611bdc241e4d40edf7d8a75
Roger Galindo <rogergalindo@MacBook-Pro-de-Roger.local>
2022-03-18 14:10:26 -0500

    function to get info from metadata

--- src/data/data_manipulator.py
+++ src/data/data_manipulator.py
@@ -34,6 +34,29 @@ def get_input_data(bucket='esp-big-data', initial_directory='BigData', filename=
  
     return data
 
+
+def get_headers_from_metadata(bucket='esp-big-data', directory='BigData', filename='metadatos-llamadas-urg-y-emer.csv'):
+    """Read a csv file in a bucket of GCS, the file must use latin1 encoding and the separator is a semicolon (;)
+
+    Args:
+        bucket (str, optional): Name of the bucket. Defaults to 'esp-big-data'.
+        directory (str, optional): project directory. Defaults to 'BigData'.
+        filename (str, optional):csv file to read. Defaults to 'datos-abiertos-agosto-2019.csv'.
+
+    Returns:
+        pandas.dataframe: dataframe with the meta data
+    """
+    logger = logging.getLogger('get_headers_from_metadata')
+
+    # get full file path from the given bucket
+    logger.info('Reading file: {}'.format(file))
+    file = 'gs://{}/{}/data/{}'.format(bucket, directory, filename)
+    df = pd.read_csv(file, encoding='latin1', sep=';')
+    logger.info('Done!!')
+
+    return df
+
+
 if __name__ == '__main__':
     # set up the logging output format (to visualize in a terminal)
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -43,4 +66,5 @@ if __name__ == '__main__':
         format = log_fmt
     )
 
+    df_metadata = get_headers_from_metadata()
     get_input_data()
