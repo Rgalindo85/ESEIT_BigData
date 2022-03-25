8308e73bcffb7ea553c83f3210f36be940adc37f
Rgalindo85 <rogerschrodinger@gmail.com>
2022-03-25 21:45:27 +0000

    included a function to calculate time between incident and response

--- src/data/data_manipulator.py
+++ src/data/data_manipulator.py
@@ -3,8 +3,14 @@ import os
 import logging
 
 import pandas as pd
+import numpy as np
 
-def get_input_data(bucket='esp-big-data', initial_directory='BigData', filename='datos-abiertos-agosto-2019.csv'):
+from dateutil.parser        import parse
+from dateutil.relativedelta import relativedelta
+
+from tqdm import tqdm
+
+def get_input_data(bucket='esp-big-data', initial_directory='BigData', step = 'raw', filename='datos-abiertos-agosto-2019.csv'):
     """Read a csv file in a bucket of GCS, the file must use latin1 encoding and the separator is a semicolon (;)
 
     Args:
@@ -25,7 +31,7 @@ def get_input_data(bucket='esp-big-data', initial_directory='BigData', filename=
     )
 
     # join the path to have the full path of the file
-    filepath = os.path.join(project_dir, 'data', 'raw')
+    filepath = os.path.join(project_dir, 'data', step)
     file     = os.path.join(filepath, filename)
 
     logger.info('Reading file: {}'.format(file))
@@ -33,6 +39,37 @@ def get_input_data(bucket='esp-big-data', initial_directory='BigData', filename=
     logger.info('Done!!')
  
     return data
+
+
+def clean_datetime_cols(df, initial_time_col = 'FECHA_INCIDENTE', final_time_col='FECHA_INICIO_DESPLAZAMIENTO_MOVIL'):
+    
+    df_date_nan   = df[df[initial_time_col].isna()]
+    df_date_nonan = df[~df[initial_time_col].isna()]
+    
+    
+    ti = df_date_nonan[initial_time_col].apply(parse)
+    tf = df_date_nonan[final_time_col].apply(parse)
+
+    deltat = tf - ti
+    list_minutes = list()
+    for delta_time in deltat.values:
+        seconds = int(delta_time)/1e9
+        list_minutes.append(seconds/60.)
+    
+    
+    list_new_dates = list()
+    for time in tqdm(df_date_nan[final_time_col].values):
+        new_date = parse(time) - relativedelta(minutes=np.median(list_minutes))
+        list_new_dates.append(new_date)
+        
+    df_date_nan[initial_time_col] = list_new_dates
+    df_out = pd.concat([df_date_nan, df_date_nonan])
+    
+    # df_out[initial_time_col] = df_out[initial_time_col].apply(lambda x: parse(x)) 
+    # df_out[final_time_col]   = df_out[final_time_col].apply(lambda x: parse(x))
+    
+    return df_out
+
 
 if __name__ == '__main__':
     # set up the logging output format (to visualize in a terminal)
